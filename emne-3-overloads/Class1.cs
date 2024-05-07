4f52ba07871f36917c72b3ead249831c5486610c
griminir <viktor.elliot.king@gmail.com>
2024-05-07 20:53:04 +0200

    Add project files.

---
+++ emne-3-overloads/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emne_3_overloads
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; } 
+
+        public Person(string name, int age) // Eksemple om jeg vet navnet og alderne 
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public Person(string name) // om jeg vet navnet men ikke alderen
+        {
+            Name = name;
+        }
+
+        public void PrintWelcomeMsg(string name = "Terje")
+        {
+            Console.WriteLine($"hei {name}");
+        } 
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"navn: {Name} alder: {Age}");
+        }
+    }
+
+}
---
+++ emne-3-overloads/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using emne_3_overloads;
+
+Console.WriteLine("Hello, World!");
+
+var Mann1 = new Person("Oskar");
+var Mann2 = new Person("kåre", 56);
+
+Mann1.PrintWelcomeMsg();
+Mann1.PrintWelcomeMsg("Henrik");
+
+Mann1.PrintInfo();
+Mann2.PrintInfo();
